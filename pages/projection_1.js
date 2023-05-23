fcc02a717d76b3ce9d17188136daeeb5d3cb6504
Audrey Acken <audreyacken@Audreys-MacBook-Pro-3.local>
2023-05-22 17:18:13 -0700

    added vector colors

--- pages/projection_1.js
+++ pages/projection_1.js
@@ -1,13 +1,14 @@
 let origin_x = 200, origin_y = 300;
 let circle_data = [{ id: 0, x: origin_x, y: origin_y }, { id: 1, x: 100, y: 100 }, { id: 2, x: 250, y: 70 }];
-let vector_data = [{ id: 0, x1: origin_x, y1: origin_y, x2: circle_data[1]["x"], y2: circle_data[1]["y"] },
-                    { id: 1, x1: origin_x, y1: origin_y, x2: circle_data[2]["x"], y2: circle_data[2]["y"] },
+let vector_data = [{ id: 0, x1: origin_x, y1: origin_y, x2: circle_data[1]["x"], y2: circle_data[1]["y"], stroke: "black" },
+                    { id: 1, x1: origin_x, y1: origin_y, x2: circle_data[2]["x"], y2: circle_data[2]["y"], stroke: "black" },
                     {
                         id: 2,
                         x1: origin_x,
                         y1: origin_y,
-                        x2: origin_x + ((circle_data[2]["x"] - origin_x) * ((circle_data[1]["x"] - origin_x) * (circle_data[2]["x"] - origin_x) + (- circle_data[1]["y"] + origin_y) * (- circle_data[2]["y"] + origin_y)) / ((circle_data[2]["x"] - origin_x) ^ 2 + (-circle_data[2]["y"] + origin_y) ^ 2)),
-                        y2: origin_y - ((- circle_data[2]["y"] + origin_y) * ((circle_data[1]["x"] - origin_x) * (circle_data[2]["x"] - origin_x) + (- circle_data[1]["y"] + origin_y) * (- circle_data[2]["y"] + origin_y)) / ((circle_data[2]["x"] - origin_x) ^ 2 + (-circle_data[2]["y"] + origin_y) ^ 2))
+                        x2: origin_x + ((circle_data[2]["x"] - origin_x) * ((circle_data[1]["x"] - origin_x) * (circle_data[2]["x"] - origin_x) + (origin_y - circle_data[1]["y"]) * (origin_y - circle_data[2]["y"])) / ((circle_data[2]["x"] - origin_x) ** 2 + (origin_y - circle_data[2]["y"]) ** 2)),
+                        y2: origin_y - ((- circle_data[2]["y"] + origin_y) * ((circle_data[1]["x"] - origin_x) * (circle_data[2]["x"] - origin_x) + (origin_y - circle_data[1]["y"]) * (origin_y - circle_data[2]["y"])) / ((circle_data[2]["x"] - origin_x) ** 2 + (origin_y - circle_data[2]["y"]) ** 2)),
+                        stroke: "red"
                     }];
 
 let drag = d3.drag()
@@ -36,14 +37,15 @@ function update() {
         .attr('opacity', 0.3)
         .attr('r', 20);
 
-    vector_data = [{ id: 0, x1: origin_x, y1: origin_y, x2: circle_data[1]["x"], y2: circle_data[1]["y"] },
-                    { id: 1, x1: origin_x, y1: origin_y, x2: circle_data[2]["x"], y2: circle_data[2]["y"] },
+    vector_data = [{ id: 0, x1: origin_x, y1: origin_y, x2: circle_data[1]["x"], y2: circle_data[1]["y"], stroke: "black" },
+                    { id: 1, x1: origin_x, y1: origin_y, x2: circle_data[2]["x"], y2: circle_data[2]["y"], stroke: "black" },
                     {
                         id: 2,
                         x1: origin_x,
                         y1: origin_y,
-                        x2: origin_x + ((circle_data[2]["x"] - origin_x) * ((circle_data[1]["x"] - origin_x) * (circle_data[2]["x"] - origin_x) + (- circle_data[1]["y"] + origin_y) * (- circle_data[2]["y"] + origin_y)) / ((circle_data[2]["x"] - origin_x) ^ 2 + (-circle_data[2]["y"] + origin_y) ^ 2)),
-                        y2: origin_y - ((- circle_data[2]["y"] + origin_y) * ((circle_data[1]["x"] - origin_x) * (circle_data[2]["x"] - origin_x) + (- circle_data[1]["y"] + origin_y) * (- circle_data[2]["y"] + origin_y)) / ((circle_data[2]["x"] - origin_x) ^ 2 + (-circle_data[2]["y"] + origin_y) ^ 2))
+                        x2: origin_x + ((circle_data[2]["x"] - origin_x) * ((circle_data[1]["x"] - origin_x) * (circle_data[2]["x"] - origin_x) + (origin_y - circle_data[1]["y"]) * (origin_y - circle_data[2]["y"])) / ((circle_data[2]["x"] - origin_x) ** 2 + (origin_y - circle_data[2]["y"]) ** 2)),
+                        y2: origin_y - ((- circle_data[2]["y"] + origin_y) * ((circle_data[1]["x"] - origin_x) * (circle_data[2]["x"] - origin_x) + (origin_y - circle_data[1]["y"]) * (origin_y - circle_data[2]["y"])) / ((circle_data[2]["x"] - origin_x) ** 2 + (origin_y - circle_data[2]["y"]) ** 2)),
+                        stroke: "red"
                     }];
 
     d3.select('svg')
@@ -55,6 +57,7 @@ function update() {
         .attr('x2', function (d) { return d.x2; })
         .attr('y2', function (d) { return d.y2; })
         .attr('stroke-width', 4)
+        .attr('stroke', function (d) { return d.stroke; })
         .attr('marker-end', "url(#arrowhead)");
 }
 
