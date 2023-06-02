acbb0f0a321290d56f9d64359975fcbc4319760c
Audrey Acken <audreyacken@Audreys-MacBook-Pro-3.local>
2023-06-02 12:22:33 -0700

    lablels

--- pages/projections.js
+++ pages/projections.js
@@ -54,9 +54,10 @@ function update() {
                         stroke: "red",
                         marker: "url(#arrowhead_red)"
         }];
-    vector_labels = [{ id: 0, x: 420, y: 30, fill: "black", text: "a = ".concat("(", (((circle_data[1]["x"] - origin_x) * (1 / 20)).toFixed(2)).toString(), ", ", (((circle_data[1]["y"] - origin_y) * (1 / 20)).toFixed(2)).toString(), ")") },
-    { id: 1, x: 420, y: 70, fill: "blue", text: "b = ".concat("(", (((circle_data[0]["x"] - origin_x) * (1 / 20)).toFixed(2)).toString(), ", ", (((circle_data[0]["y"] - origin_y) * (1 / 20)).toFixed(2)).toString(), ")") },
-    { id: 2, x: 420, y: 110, fill: "red", text: "proj = ".concat("(", ((circle_data[1]["x"] - origin_x) * (1 / 20)).toString(), ", )") }
+    vector_labels = [{ id: 0, x: 420, y: 30, fill: "black", text: "a = ".concat("(", (((circle_data[1]["x"] - origin_x) * (1 / 20)).toFixed(2)).toString(), ", ", (-((circle_data[1]["y"] - origin_y) * (1 / 20)).toFixed(2)).toString(), ")") },
+    { id: 1, x: 420, y: 70, fill: "blue", text: "b = ".concat("(", (((circle_data[0]["x"] - origin_x) * (1 / 20)).toFixed(2)).toString(), ", ", (-((circle_data[0]["y"] - origin_y) * (1 / 20)).toFixed(2)).toString(), ")") },
+    { id: 2, x: 420, y: 110, fill: "red", text: "proj = ".concat("(", ((circle_data[1]["x"] - origin_x) * (1 / 20)).toString(), ", )") },
+    { id: 3, x: 420, y: 150, fill: "black", text: "a • b = " }
     ];
 
     d3.select('svg')
